57e8a2ebb316fe4c02ca769a01fb271f8a285783
LaudRam <laudiciaramasenya@gmail.com>
2023-11-23 13:45:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -92,10 +92,9 @@ def main():
               else:
                      st.write(f"The severity prediction is slight injury")
                
-              st.write("Developed By: Avi kumar Talaviya")
-              st.markdown("""Reach out to me on: [Twitter](https://twitter.com/avikumart_) |
-              [Linkedin](https://www.linkedin.com/in/avi-kumar-talaviya-739153147/) |
-              [Kaggle](https://www.kaggle.com/avikumart) 
+              st.write("Developed By: Laudicia Ramasenya")
+              st.markdown("""Reach out to me on: [Twitter](https://twitter.com/Laud_Rama) |
+              [Linkedin](https://www.linkedin.com/in/laudicia-ramasenya-98305312a/)
               """)
 
 # App.py - Webpage Aesthetics
@@ -120,7 +119,7 @@ The task is to classify this variable based on the other 31 features step-by-ste
 The metric for evaluation will be f1-score
 """)
 
-st.markdown("Please find GitHub repository link of project: [Click Here](https://github.com/avikumart/Road-Traffic-Severity-Classification-Project)")                  
+st.markdown("Please find GitHub repository link of project: [Click Here](https://github.com/LaudRam/Laud-streamlit)")                  
    
 # run the main function               
 if __name__ == '__main__':
